4146b388dc092960d94530ec0df0bf285cb247fc
Maciej Mensfeld <maciej@mensfeld.pl>
2025-06-21 21:32:33 +0200

    test alternative build flow

--- ext/build_macos_arm64.sh
+++ ext/build_macos_arm64.sh
@@ -356,8 +356,7 @@ make clean || true
 #}
 
 # Build with full static library paths
-# Build with full static library paths INCLUDING the C library
-make -j$(get_cpu_count) src/librdkafka.a src/librdkafka.1.dylib \
+make -j$(get_cpu_count) librdkafka.a \
     LDFLAGS="$LDFLAGS -L$OPENSSL_PREFIX/lib -L$SASL_PREFIX/lib -L$ZLIB_PREFIX/lib -L$ZSTD_PREFIX/lib" \
     LIBS="-lssl -lcrypto -lsasl2 -lz -lzstd"
 
