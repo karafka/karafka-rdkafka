0df681a788a493f7e021495558c1dcc443513d9a
Maciej Mensfeld <maciej@mensfeld.pl>
2025-06-22 17:32:57 +0200

    remarks

--- spec/rdkafka/metadata_spec.rb
+++ spec/rdkafka/metadata_spec.rb
@@ -31,7 +31,6 @@ describe Rdkafka::Metadata do
         expect(subject.brokers.length).to eq(1)
         expect(subject.brokers[0][:broker_id]).to eq(1)
         expect(%w[127.0.0.1 localhost]).to include(subject.brokers[0][:broker_name])
-        expect(subject.brokers[0][:broker_name]).to be_in(%w[127.0.0.1 localhost])
         expect(subject.brokers[0][:broker_name]).to eq("127.0.0.1")
         expect(subject.brokers[0][:broker_port]).to eq(9092)
       end
